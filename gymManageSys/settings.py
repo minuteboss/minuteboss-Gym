bd9197f490040f6ef3c948f6f9e80dfaf9f7482b
minuteboss <90935170+minuteboss@users.noreply.github.com>
2022-07-06 01:00:54 +0300

    Update settings.py

--- gymManageSys/settings.py
+++ gymManageSys/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,6 +150,6 @@ import django_heroku
 TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
 
